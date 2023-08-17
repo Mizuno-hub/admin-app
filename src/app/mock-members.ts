c84267eaeae8d5875123b0df10adfdd5116a7491
Mizuno_hub <mizunoforbusiness@gmail.com>
2023-08-18 00:14:50 +0900

    MEMBERSデータを定義

--- src/app/mock-members.ts
+++ src/app/mock-members.ts
@@ -1,6 +1,6 @@
 import { Member } from "./member";
 
-const MEMBERS: Member[] = [
+export const MEMBERS: Member[] = [
     { id: 11, name: '武山 岳大' },
     { id: 12, name: '駒倉 光紀' },
     { id: 13, name: '長田 研太' },
@@ -11,4 +11,4 @@ const MEMBERS: Member[] = [
     { id: 18, name: '大槻 祐大' },
     { id: 19, name: '岩野 紀之' },
     { id: 20, name: '佐々木 小次郎' }
-]
+];
