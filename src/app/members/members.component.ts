fed75aed3431517127909d1e8794462d935bd88c
Mizuno_hub <mizunoforbusiness@gmail.com>
2023-08-20 23:12:43 +0900

    社員リストを選択可能に

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -15,10 +15,16 @@ export class MembersComponent implements OnInit{
     name: '田中太郎'
   };
 
+  selectedMember!: Member;
+
   constructor(){}
 
   ngOnInit(): void {
       
+  }
+
+  onSelect(member: Member): void{
+    this.selectedMember = member;
   }
 
 }
